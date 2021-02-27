05787750aa1b02dc86094e1cab7f31b97b24619f
Daniel Bejarano <dbejarano820@gmail.com>
2021-02-27 15:55:45 -0600

    nada

---
+++ lista.h
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Nodo{
+
+    int dato;
+    //cualquier tipo de dato adicionales
+
+    Nodo* siguiente;
+
+    Nodo(){
+        dato = 0;
+        siguiente = NULL;
+    }
+
+    Nodo(int pDato){
+        dato = pDato;
+        siguiente = NULL;
+    }
+
+    Nodo(int pDato, Nodo* pNodo){
+        dato = pDato;
+        siguiente = pNodo;
+    }
+};
+
+
+class listaSimple{
+
+    public:
+
+    Nodo* primerNodo; //first node
+    Nodo* ultimoNodo; //last node
+
+    listaSimple(){
+        primerNodo = ultimoNodo = NULL;
+    }
+
+    bool isEmpty();
+    void insertFirst(Nodo*);
+    void insertLast(Nodo*);
+    int largo();
+    //bool esta();
+
+};
