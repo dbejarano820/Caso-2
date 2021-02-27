eadc83015b77d388db4c88dc6a5cc43c9165c906
MauAguero-12 <76414021+MauAguero-12@users.noreply.github.com>
2021-02-27 15:53:34 -0600

    Add files via upload

---
+++ repartidores.h
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class repartidor{
+    protected:
+    int distanciaAlRestaurante;
+    int speed;
+    string tipoRepartidor;
+
+    public:
+    int calculateTime(int pDistanciaPedido){
+        return (distanciaAlRestaurante + pDistanciaPedido) / speed;
+    }
+
+    void setDistanciaAlRestaurante(int pDistancia){
+        distanciaAlRestaurante = pDistancia;
+    }
+};
+
+class bici : public repartidor{
+    public:
+    bici(){
+        speed = 10;
+        tipoRepartidor = "Bici";
+    }
+};
+
+class moto : public repartidor{
+    public:
+    moto(){
+        speed = 20;
+        tipoRepartidor = "Moto";
+    }
+};
+
+class carro : public repartidor{
+    public:
+    carro(){
+        speed = 30;
+        tipoRepartidor = "Carro";
+    }
+};
