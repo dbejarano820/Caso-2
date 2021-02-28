0cb9760064737fb87c7c0f11984ebdb7449b7622
Daniel Bejarano <dbejarano820@gmail.com>
2021-02-27 18:00:16 -0600

    ok

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
 #include "lista.h"
-#include "restaurantes.h"
-//probando
+
 
 int main(){
 
 
     listaSimple* lista = new listaSimple();
     lista->insertFirst(new Nodo(4));
-<<<<<<< HEAD
+    cout << "Hola mundo" << endl;
+    //hola
     cout << "FUNCIONA DE ESTA MANERA" << endl;
-=======
->>>>>>> a1a1ad867728fc95cfc6072abdcf8a713462dc12
 
-    Restaurante * res = new Restaurante();
-    res->setDistanciaBici(15);
-    cout << res->repBici->distanciaAlRestaurante << endl;
-
-    cout << lista->primerNodo->dato << endl;
-    cout << "funciona" << endl;
 
     return 0;
 }
